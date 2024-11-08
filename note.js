df7dd8963a1eb42e6fd8bacbb1fefe43712f5179
akshat vishal <akshatvishal05@gmail.com>
2024-11-08 15:57:00 +0530

    error fix

--- note.js
+++ note.js
@@ -1,110 +1,114 @@
-const creatbtn = document.querySelector('.creatbtn');
-const welcome=document.querySelector('.welcome')
-const colorOptions = document.querySelectorAll('.color-option');
-const list = document.querySelector('#list'); 
-const pop = document.querySelector('.popup');
-const form2=document.querySelector('form')
-const edit=document.querySelector('.edit');
-const mode=document.querySelector('.darkmode input[type="checkbox"]');
-const header=document.querySelector('.header');
-let currentnote=null;
-var color='#FF0000'
-const currentUser=JSON.parse(localStorage.getItem("currentUser"));
+const creatbtn = document.querySelector(".creatbtn");
+const welcome = document.querySelector(".welcome");
+const colorOptions = document.querySelectorAll(".color-option");
+const list = document.querySelector("#list");
+const pop = document.querySelector(".popup");
+const form2 = document.querySelector("form");
+const edit = document.querySelector(".edit");
+const mode = document.querySelector('.darkmode input[type="checkbox"]');
+const header = document.querySelector(".header");
+let currentnote = null;
+var color = "#FF0000";
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 console.log(currentUser);
-if(currentUser){
-    console.log(currentUser);
-    welcome.innerHTML= `<h1 class="welcome">Welcome ${currentUser.username}</h1>`
+if (currentUser) {
+  console.log(currentUser);
+  welcome.innerHTML = `<h1 class="welcome">Welcome ${currentUser.username}</h1>`;
 }
 
 //loadnote
-currentUser.list.forEach((element)=>{
-    creatNote(element)
-})
+currentUser.list.forEach((element) => {
+  creatNote(element);
+});
 
 //Darkmode
 
-mode.addEventListener('change',(e)=>{
-    if(mode.checked){
-        document.body.style.backgroundColor='black';
-        header.style.backgroundColor='#170F55';
-    }
-    else{
-        document.body.style.backgroundColor='white'
-        header.style.backgroundColor='#3B28CC';
-    }
-})
+mode.addEventListener("change", (e) => {
+  if (mode.checked) {
+    document.body.style.backgroundColor = "black";
+    header.style.backgroundColor = "#170F55";
+  } else {
+    document.body.style.backgroundColor = "white";
+    header.style.backgroundColor = "#3B28CC";
+  }
+});
 //colorchoice
 
-colorOptions.forEach(option => {
-    option.addEventListener('click', (event) => {
-       color= event.target.getAttribute('data-color'); 
-       pop.style.backgroundColor=color;
-    });
+colorOptions.forEach((option) => {
+  option.addEventListener("click", (event) => {
+    color = event.target.getAttribute("data-color");
+    pop.style.backgroundColor = color;
+  });
 });
 
 //popup
 
-window.addEventListener('keydown', (e) => {
-    if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'z') {
-         creatbtn.click();
-    }
+window.addEventListener("keydown", (e) => {
+  if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "z") {
+    creatbtn.click();
+  }
 });
 
-creatbtn.addEventListener('click',(e)=>{
-    pop.style.display='block';
-})
-
-
+creatbtn.addEventListener("click", (e) => {
+  pop.style.display = "block";
+});
 
 //notecreation
 
-form2.addEventListener("submit",(e)=>{
-    e.preventDefault()
-        const title=form2.title.value;
-        const category=form2.category.value;
-        const content=form2.content.value;
-        const Notecolor=color;
-    if(currentnote){
-        currentnote=null;
-    }
-    else{
-        const Newnote={title, category , content , Notecolor};
-        currentUser.list.push(Newnote);
-        creatNote(Newnote)
-        localStorage.setItem("currentUser", JSON.stringify(currentUser));
-        form2.reset();
-        pop.style.display = 'none'; 
-    }
-})
+form2.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = form2.title.value;
+  const category = form2.category.value;
+  const content = form2.content.value;
+  const Notecolor = color;
+  if (currentnote) {
+    currentnote = null;
+  } else {
+    const Newnote = { title, category, content, Notecolor };
+    currentUser.list.push(Newnote);
+    creatNote(Newnote);
+    localStorage.setItem("currentUser", JSON.stringify(currentUser));
+    form2.reset();
+    pop.style.display = "none";
+  }
+});
 
-function creatNote(note){
-    let newnote = document.createElement('div');
-        newnote.innerHTML = `
+function creatNote(note) {
+  let newnote = document.createElement("div");
+  newnote.innerHTML = `
                 <h2 tabindex="1">${note.title}</h2>
                 <p>${note.category}</p>
                 <span class="edit">Edit</span>
                 <span class="close">x</span>
-                <textarea placeholder="text here..." ro ws="10" cols="30">${note.content}</textarea> `;
-                newnote.classList.add('note');
-                newnote.style.backgroundColor=note.Notecolor  
-                list.appendChild(newnote);
-                localStorage.setItem("currentUser", JSON.stringify(currentUser));
-                loadinfo();
+                `;
+  newnote.classList.add("note");
+  newnote.style.backgroundColor = note.Notecolor;
+  list.appendChild(newnote);
+  localStorage.setItem("currentUser", JSON.stringify(currentUser));
+  loadinfo();
 }
 
-// Update login info
-function loadinfo(){
-    let users = JSON.parse(localStorage.getItem("form1")) || []; 
+note.addEventListener("click",(e)=>{
+  if(e.target==edit){
+    currentnote=e
+  }
+})
 
-    users.forEach((user, index) => {
-        // Check if the username and email match
-        if (user.username === currentUser.username && user.email === currentUser.email) {
-            users.splice(index, 1); 
-            console.log(index)
-        }
-    });
-    
-    users.push(currentUser);
-    localStorage.setItem("form1", JSON.stringify(users));
-    
+// Update login info
+function loadinfo() {
+  let users = JSON.parse(localStorage.getItem("form1")) || [];
+
+  users.forEach((user, index) => {
+    // Check if the username and email match
+    if (
+      user.username === currentUser.username &&
+      user.email === currentUser.email
+    ) {
+      users.splice(index, 1);
+      console.log(index);
+    }
+  });
+
+  users.push(currentUser);
+  localStorage.setItem("form1", JSON.stringify(users));
 }
--- notes.js
+++ notes.js
@@ -1,126 +1,105 @@
-        const creatbtn = document.querySelector('.creatbtn');
-        const colorOptions = document.querySelectorAll('.color-option');
-        const list = document.querySelector('#list'); 
-        const pop = document.querySelector('.popup');
-        const form2=document.querySelector('form')
-        const edit=document.querySelector('.edit');
-        const mode=document.querySelector('.darkmode input[type="checkbox"]');
-        const header=document.querySelector('.header')
-        let time=document.querySelector('.Editedok');
-        let created=document.querySelector('.created');
+const creatbtn = document.querySelector(".creatbtn");
+const colorOptions = document.querySelectorAll(".color-option");
+const list = document.querySelector("#list");
+const pop = document.querySelector(".popup");
+const form2 = document.querySelector("form");
+const edit = document.querySelector(".edit");
+const mode = document.querySelector('.darkmode input[type="checkbox"]');
+const header = document.querySelector(".header");
+let time = document.querySelector(".Editedok");
+let created = document.querySelector(".created");
 
-        var color;
-        let currentnote=null;
+var color;
+let currentnote = null;
 
+const obj = JSON.parse(localStorage.getItem("form"));
+console.log(obj);
+const nameelement = document.querySelector(".welcome");
+nameelement.innerHTML = `<h1 class="welcome">Welcome ${obj.Username}</h1>`;
 
-        
-        const obj = JSON.parse(localStorage.getItem('form'));
-        console.log(obj);
-        const nameelement = document.querySelector('.welcome');
-        nameelement.innerHTML = `<h1 class="welcome">Welcome ${obj.Username}</h1>`;
+creatbtn.addEventListener("click", () => {
+  pop.style.display = "block";
+});
 
-        creatbtn.addEventListener('click', () => {
-            pop.style.display='block';
-        });
+colorOptions.forEach((option) => {
+  option.addEventListener("click", (event) => {
+    color = event.target.getAttribute("data-color");
+    pop.style.backgroundColor = color;
+  });
+});
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("close")) {
+    e.target.parentElement.remove();
+  } else if (e.target.classList.contains("edit")) {
+    currentnote = e.target.parentElement;
+    const title = currentnote.querySelector("h2").textContent;
+    const content = currentnote.querySelector("textarea").value;
+    const category = currentnote.querySelector("p").textContent;
 
-        colorOptions.forEach(option => {
-                option.addEventListener('click', (event) => {
-                   color= event.target.getAttribute('data-color'); 
-                   pop.style.backgroundColor=color;
-                });
-        });
-        list.addEventListener('click', (e) => {
-            if (e.target.classList.contains('close')) {
-                e.target.parentElement.remove(); 
-            } else if (e.target.classList.contains('edit')) {
-            
-                currentnote = e.target.parentElement; 
-                const title = currentnote.querySelector('h2').textContent;
-                const content = currentnote.querySelector('textarea').value;
-                const category = currentnote.querySelector('p').textContent;
-                
-                form2.title.value = title;
-                form2.content.value = content;
-                form2.category.value = category;
-                pop.style.display = 'block';
-            }
-        }
-    );
+    form2.title.value = title;
+    form2.content.value = content;
+    form2.category.value = category;
+    pop.style.display = "block";
+  }
+});
 
-        form2.addEventListener("submit", (e) => {
-            const title = form2.title.value;
-            const content = form2.content.value;
-            const category = form2.category.value;
-            if(currentnote)
-            {
-                e.preventDefault();
-                currentnote.querySelector('h2').textContent = title;
-                currentnote.querySelector('textarea').value = content;
-                currentnote.querySelector('p').textContent=category;
-                currentnote.style.backgroundColor = color; 
-                form2.reset();
-                pop.style.display='none';
-                const currentDate = new Date();
-                const day = String(currentDate.getDate()).padStart(2, '0'); z
-                const month = String(currentDate.getMonth() + 1).padStart(2, '0'); 
-                const year = currentDate.getFullYear(); 
-                const hours = String(currentDate.getHours()).padStart(2, '0');
-                const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-                const seconds = String(currentDate.getSeconds()).padStart(2, '0');
-                
-                const formattedDate = `${day}.${month}.${year}`;
-                console.log(formattedDate); 
-                time.innerHTML=`
-                <h3 class="time">Last Edited : ${formattedDate} ${hours}:${minutes}:${seconds}</h3>`
-                
-            }
-            else{
-                e.preventDefault();
-                console.log("Create button clicked");
-            let newnote = document.createElement('div');
-            newnote.innerHTML = `
+form2.addEventListener("submit", (e) => {
+  const title = form2.title.value;
+  const content = form2.content.value;
+  const category = form2.category.value;
+  if (currentnote) {
+    e.preventDefault();
+    currentnote.querySelector("h2").textContent = title;
+    currentnote.querySelector("textarea").value = content;
+    currentnote.querySelector("p").textContent = category;
+    currentnote.style.backgroundColor = color;
+    form2.reset();
+    pop.style.display = "none";
+    const currentDate = new Date();
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    z;
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const year = currentDate.getFullYear();
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+    const formattedDate = `${day}.${month}.${year}`;
+    console.log(formattedDate);
+    time.innerHTML = `
+                <h3 class="time">Last Edited : ${formattedDate} ${hours}:${minutes}:${seconds}</h3>`;
+  } else {
+    e.preventDefault();
+    console.log("Create button clicked");
+    let newnote = document.createElement("div");
+    newnote.innerHTML = `
                 <h2 tabindex="1">${title}</h2>
                 <p>${category}</p>
                 <span class="edit">Edit</span>
                 <span class="close">x</span>
                 <textarea placeholder="text here..." rows="10" cols="30">${content}</textarea> `;
-                const currentDate = new Date();
-                const day = String(currentDate.getDate()).padStart(2, '0'); 
-                const month = String(currentDate.getMonth() + 1).padStart(2, '0'); 
-                const year = currentDate.getFullYear(); 
-                const formattedDate = `${day}.${month}.${year}`;
-                created.innerHTML=`
-                 <h3 class="created">${formattedDate}</h3>`
-                newnote.classList.add('note');
-                newnote.style.backgroundColor=color  
-                list.appendChild(newnote);
-                
-            form2.reset();
-            pop.style.display = 'none';   
-            }
-              
-        })    
-        
-        mode.addEventListener('change',(e)=>{
-            if(mode.checked){
-                document.body.style.backgroundColor='black';
-                header.style.backgroundColor='#170F55';
-            }
-            else{
-                document.body.style.backgroundColor='white'
-                header.style.backgroundColor='#3B28CC';
-            }
-        })
-        
+    const currentDate = new Date();
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const year = currentDate.getFullYear();
+    const formattedDate = `${day}.${month}.${year}`;
+    created.innerHTML = `
+                 <h3 class="created">${formattedDate}</h3>`;
+    newnote.classList.add("note");
+    newnote.style.backgroundColor = color;
+    list.appendChild(newnote);
 
-        
-        
-            
+    form2.reset();
+    pop.style.display = "none";
+  }
+});
 
-
-        
-        
-        
-       
-            
-            
+mode.addEventListener("change", (e) => {
+  if (mode.checked) {
+    document.body.style.backgroundColor = "black";
+    header.style.backgroundColor = "#170F55";
+  } else {
+    document.body.style.backgroundColor = "white";
+    header.style.backgroundColor = "#3B28CC";
+  }
+});
